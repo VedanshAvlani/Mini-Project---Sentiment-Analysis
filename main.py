d64785be487ae170872019148cb94f6ca397d54e
Aryan <81212535+Aryan4412@users.noreply.github.com>
2022-05-13 10:28:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 from array import *
+from selenium import webdriver
+from selenium.common.exceptions import *
+
+driver = webdriver.Chrome("C:/Users/aryan/Downloads/chromedriver_win32 (2)/chromedriver.exe")
+
+
+
+
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+driver.implicitly_wait(0.5)
+
+
+driver.get("https://www.google.com/")
+
+m = driver.find_element_by_name("q")
+
+s=input(("enter match \n"))
+
+m.send_keys(s)
+
+time.sleep(0.2)
+
+m.send_keys(Keys.ENTER)
 
 api_key = 'SeSHtUbPmmuKvzudEu0sCjwhE'
 api_key_secret = 'pNfT1G0W1c8Dm2fUYWGVFDHdImIQTXH9uUtcEWddQsXEY5ZlG0'
@@ -15,14 +41,14 @@ authenticator.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(authenticator, wait_on_rate_limit=True)
 
-
+c=[]
 b=array('i',[])
-for i in range(2):
-    a=input(print("enter player name"))
-
+for i in range(3):
+    a=input(("enter player name \n"))
+    c.append(a)
     search= f'#{a} -filter:retweets lang:en'
 
-    tweet_cursor = tweepy.Cursor(api.search_tweets, q=search, tweet_mode='extended').items(10)
+    tweet_cursor = tweepy.Cursor(api.search_tweets, q=search, tweet_mode='extended').items(100)
 
     tweets = [tweet.full_text for tweet in tweet_cursor]
 
@@ -33,6 +59,8 @@ for i in range(2):
         row['tweets'] = re.sub('#\S+', '', row['Tweets'])
         row['tweets'] = re.sub('@\S+', '', row['Tweets'])
         row['tweets'] = re.sub('\\n', '', row['Tweets'])
+
+
     #print(tweets_df)
     #print(tweets)
 
@@ -42,10 +70,11 @@ for i in range(2):
 
     positive = tweets_df[tweets_df.Result == '+'].count()['Tweets']
     negative = tweets_df[tweets_df.Result == '-'].count()['Tweets']
-#    print(b)
+    #print(b)
     b.append(positive)
     print(positive)
-    print(negative)
+    #print(negative)
+
     langs = ['Positive', 'Negative']
     students = [positive,negative]
     a=plt.bar(langs,students)
@@ -58,12 +87,52 @@ for i in range(2):
     plt.legend
 
     plt.show()
-    print(i)
 
-if(b[0]>b[1]):
-    print('player 1 is better')
+
+data = {c[0]: b[0], c[1]: b[1], c[2]: b[2]}
+players = list(data.keys())
+score = list(data.values())
+
+fig = plt.figure(figsize=(8, 5))
+
+
+plt.bar(players, score, color=['navy', 'grey', 'cyan'],edgecolor='black',width=0.4)
+
+plt.ylabel("Positivity score")
+plt.title("Comparison of player fan sentiment")
+plt.show()
+
+
+
+if (b[0] >= b[1]) and (b[0] >= b[2]):
+   largest = b[0]
+elif (b[1] >= b[0]) and (b[1] >=b[2]):
+   largest = b[1]
+elif (b[0]==b[1]and (b[1]==b[2])):
+   print(' ALL 3 PLAYERS HAD EQUALLY GOOD PERFORMANCE')
+   quit()
+elif (b[0] == b[1]):
+   print(' PLAYER 1 AND 2 HAD EQUALLY GOOD PERFORMANCE')
+   quit()
+elif (b[1] == b[2]):
+   print(' PLAYER 2 AND 3 HAD EQUALLY GOOD PERFORMANCE')
+   quit()
+else:
+   largest = b[2]
+
+if(largest==b[0]):
+    {
+        print('Player 1 has the best performance')
+
+    }
+elif(largest==b[1]):
+    {
+        print('Player 2 has the best performance')
+
+    }
 
 else:
-    print('player 2 is better' )
+    {
+        print('Player 3 has the best performance')
 
-
+    }
